96fca6fe0d356a89c718ac48f6ffd5cd234fe0f9
Gabriel Rockson <gabrielrockson99@gmail.com>
2024-12-03 07:47:24 +0000

    feat: solve day one

---
+++ 2024/day-one/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("./input.txt")
+	if err != nil {
+		fmt.Printf("An error occurred opening the file %v", err)
+	}
+
+	r := bufio.NewReader(f)
+
+	var listA []int
+	var listB []int
+	for {
+		line, err := r.ReadString(byte('\n'))
+		if err != nil {
+			break
+		}
+
+		nums := strings.Split(line, ",")
+
+		num1, _ := strconv.Atoi(nums[0])
+		listA = append(listA, num1)
+
+		num2, err := strconv.Atoi(strings.Split(nums[1], "\n")[0])
+		listB = append(listB, num2)
+
+	}
+
+	slices.SortFunc(listA, func(a, b int) int {
+		return a - b
+	})
+
+	slices.SortFunc(listB, func(a, b int) int {
+		return a - b
+	})
+
+	var totalDiff int
+	for idx, num := range listA {
+		totalDiff = totalDiff + (int(math.Abs(float64(num - listB[idx]))))
+	}
+
+	fmt.Println("Total Diff ", totalDiff)
+
+	var similarityScore int
+	for _, num := range listA {
+		var numOfAppearance int
+		for _, snum := range listB {
+			if num == snum {
+				numOfAppearance += 1
+			}
+		}
+
+		similarityScore = similarityScore + (num * numOfAppearance)
+	}
+
+	fmt.Println("Similarity Score ", similarityScore)
+
+}
